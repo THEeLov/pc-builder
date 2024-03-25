c79dc80950b41e9db8ff42fb41be15e1e81abd65
xvavro2 <xvavro2@fi.muni.cz>
2024-03-25 19:26:38 +0100

    feat: added CI/CD into Gitlab linting, formatting, testing, build

--- apps/backend-e2e/src/backend/backend.spec.ts
+++ apps/backend-e2e/src/backend/backend.spec.ts
@@ -1,10 +1,8 @@
-import axios from "axios"
-
 describe("Placeholder E2E Test", () => {
   it("should pass without any implementation", async () => {
     // This is a placeholder test that always passes.
     // You can replace this with actual tests later.
-    expect(true).toBe(true);
+    expect(true).toBe(true)
   })
 })
 
