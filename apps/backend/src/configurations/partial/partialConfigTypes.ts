c3b07c6651837d5f9cb81058563e0e69f99b5225
Juan Marcos <xmarcos@fi.muni.cz>
2024-04-28 22:25:11 +0200

    feat: formatting like a boss

--- apps/backend/src/configurations/partial/partialConfigTypes.ts
+++ apps/backend/src/configurations/partial/partialConfigTypes.ts
@@ -1,19 +1,18 @@
-import {ConfigurationType, RAM, Storage} from '@prisma/client'
-
+import { ConfigurationType, RAM, Storage } from "@prisma/client"
 
 export type PartialConfigCreate = {
-    userId: number,
+    userId: number
     configurationType: ConfigurationType
 }
 
 export type PartielaConfigEdit = {
-    userId?: number,
-    configurationType?: ConfigurationType,
-    motherboardId?: number,
-    peocessorId?: number,
-    gpuId?: number,
-    powerSupplyId?: number,
-    PCCaseId?: number,
-    rams?: RAM[],
+    userId?: number
+    configurationType?: ConfigurationType
+    motherboardId?: number
+    peocessorId?: number
+    gpuId?: number
+    powerSupplyId?: number
+    PCCaseId?: number
+    rams?: RAM[]
     storages?: Storage[]
 }
