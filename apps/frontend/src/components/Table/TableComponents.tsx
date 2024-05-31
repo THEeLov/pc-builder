8c66ece2385ec58f85e2b654a2103688cd5c45f0
Filip Kozik <536410@mail.muni.cz>
2024-06-01 00:37:37 +0200

    fix: table decomposition

--- apps/frontend/src/components/Table/TableComponents.tsx
+++ apps/frontend/src/components/Table/TableComponents.tsx
@@ -1,11 +1,8 @@
 import CustomButton from "../CustomButton/CustomButton"
 import { TiPlus } from "react-icons/ti"
-import { useRef, useState } from "react"
-import { SearchOutlined } from "@ant-design/icons"
 import type { InputRef, TableColumnsType, TableColumnType } from "antd"
-import { Button, Input, Space, Table } from "antd"
-import type { FilterDropdownProps } from "antd/es/table/interface"
-import Highlighter from "react-highlight-words"
+import { Table } from "antd"
+import { useSearch } from "./useSearch"
 
 type DataType = {
     component: {
@@ -22,95 +19,8 @@ type TableComponentsProps = {
 }
 
 const TableComponents: React.FC<TableComponentsProps> = ({ fetchedData }) => {
-    const [searchText, setSearchText] = useState("")
-    const [searchedColumn, setSearchedColumn] = useState("")
-    const searchInput = useRef<InputRef>(null)
 
-    const handleSearch = (selectedKeys: string[], confirm: FilterDropdownProps["confirm"], dataIndex: DataIndex) => {
-        confirm()
-        setSearchText(selectedKeys[0])
-        setSearchedColumn(dataIndex)
-    }
-
-    const handleReset = (clearFilters: () => void) => {
-        clearFilters()
-        setSearchText("")
-    }
-
-    const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<DataType> => ({
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-            <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-                <Input
-                    ref={searchInput}
-                    placeholder={`Search ${dataIndex}`}
-                    value={selectedKeys[0]}
-                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-                    onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-                    style={{ marginBottom: 8, display: "block" }}
-                />
-                <Space>
-                    <Button
-                        type="primary"
-                        onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-                        icon={<SearchOutlined />}
-                        size="small"
-                        style={{ width: 90 }}
-                    >
-                        Search
-                    </Button>
-                    <Button
-                        onClick={() => clearFilters && handleReset(clearFilters)}
-                        size="small"
-                        style={{ width: 90 }}
-                    >
-                        Reset
-                    </Button>
-                    <Button
-                        type="link"
-                        size="small"
-                        onClick={() => {
-                            confirm({ closeDropdown: false })
-                            setSearchText((selectedKeys as string[])[0])
-                            setSearchedColumn(dataIndex)
-                        }}
-                    >
-                        Filter
-                    </Button>
-                    <Button
-                        type="link"
-                        size="small"
-                        onClick={() => {
-                            close()
-                        }}
-                    >
-                        close
-                    </Button>
-                </Space>
-            </div>
-        ),
-        filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />,
-        onFilter: (value, record) =>
-            record.component[dataIndex]
-                .toString()
-                .toLowerCase()
-                .includes((value as string).toLowerCase()),
-        onFilterDropdownOpenChange: (visible) => {
-            if (visible) {
-                setTimeout(() => searchInput.current?.select(), 100)
-            }
-        },
-        render: (text) =>
-            searchedColumn === dataIndex ? (
-                <Highlighter
-                    highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-                    searchWords={[searchText]}
-                    autoEscape
-                    textToHighlight={text ? text.toString() : ""}
-                />
-            ) : (
-                text
-            ),
-    })
+    const { getColumnSearchProps } = useSearch()
 
     const columns: TableColumnsType<DataType> = [
         {
