f0796d06225d3a9f91225561b1614a8bedd0a004
Filip Kozik <536410@mail.muni.cz>
2024-06-04 10:28:34 +0200

    feat: register auth

--- apps/frontend/src/Pages/Register/Register.tsx
+++ apps/frontend/src/Pages/Register/Register.tsx
@@ -4,13 +4,23 @@ import { z } from "zod"
 import Bob from "../../images/sign_UP_bob.png"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { schema } from "./validation"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useRegister } from "@/hooks/useAuth"
+import { useAuth } from "@/auth/AuthProvider"
 
 type FormFields = z.infer<typeof schema>
 
+const mockRegister = {
+    id: "123141421",
+    username: "Filipko",
+    email: "filipko@gmail.com",
+    role: "USER",
+}
+
 const Register = () => {
     const { mutateAsync: RegisterMutation } = useRegister()
+    const { login } = useAuth();
+    const navigate = useNavigate();
 
     const {
         register,
@@ -27,7 +37,10 @@ const Register = () => {
             return
         }
         try {
-            const response = await RegisterMutation(data)
+            // const response = await RegisterMutation(data)
+            const response = mockRegister;
+            login(response);
+            navigate("/")
         } catch (error) {
             setError("root", {
                 message: "Something went wrong.",
